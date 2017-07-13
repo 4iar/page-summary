3116a3718aee37830f803f9da11313a761de2018
4iar <me@4iar.com>
2017-07-13 16:39:42 +0100

    Implement requests

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import sys
+import requests
 from src.page_analyser import Page
 
+
 def get_page(url):
-    '''Return the page at the given url as text
-    '''
-    return
+    '''Return the page at the given url as text'''
+    req = requests.get(url)
+    return req.text
 
-
-if __name__ == 'main':
+if __name__ == '__main__':
     body = get_page(sys.argv[1])
     page = Page(body)
-    body.run_analysis()
+    page.analyse()
